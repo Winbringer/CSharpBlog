ea3b266404af817d9ade6bcb30493b35f46ba3b2
Winbringer <voin1213@yandex.ru>
2015-12-13 04:44:51 +0600

    Edit_Atction

--- ДвижокНовостейЗМ/Controllers/ReplyController.cs
+++ ДвижокНовостейЗМ/Controllers/ReplyController.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using ДвижокНовостейЗМ.Models;
 
@@ -9,7 +11,57 @@ namespace ДвижокНовостейЗМ.Controllers
     {
         ApplicationDBContext db = new ApplicationDBContext();
 
-       
+        public ActionResult Edit(int? id)
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           Reply reply = db.Replys.Find(id);
+            if (reply == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reply);            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Text")] Reply reply)
+        {
+            Reply oldReply = db.Replys.Find(reply.Id);
+
+            oldReply.Text = reply.Text;
+            oldReply.Date = DateTime.Now;
+            db.SaveChanges();
+           return RedirectToAction("Details","Messages", new { id = oldReply.Message.Id });            
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           Reply reply = db.Replys.Find(id);
+            if (reply == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reply);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+           Reply reply = db.Replys.Find(id);
+            db.Replys.Remove(reply);
+            db.SaveChanges();
+            return RedirectToAction("Details", "Messages", new { id = oldReply.Message.Id });
+        }
         protected override void Dispose(bool disposing)
         {
             if (disposing)
